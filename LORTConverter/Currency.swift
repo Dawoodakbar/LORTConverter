9f2951f8343457293a6b625f71ab24cb0c7d7d8b
Dawood <dawood.3techsoft@gmail.com>
2025-09-08 17:44:46 +0500

    Made a currency model

---
+++ LORTConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LORTConverter
+//
+//  Created by dawood on 9/8/25.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
